e869c29e912efb50611146400a907759f259977d
Andres Rodriguez <andr1910es@gmail.com>
2018-01-29 21:45:55 -0500

    script.js now changes the aspect ratio of google calendar based on viewport

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 var width = viewportSize.getWidth();
 
 if(width <= 390){
-    document.querySelector(".navbar-toggler").classList.add("mx-auto")
+    document.querySelector(".navbar-toggler").classList.add("mx-auto");
+    document.querySelector(".embed-responsive").classList.remove("embed-responsive-16by9");
+    document.querySelector(".embed-responsive").classList.add("embed-responsive-1by1");
 }
